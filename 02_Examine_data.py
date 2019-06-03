40fbfad65ef06712f75c052e58fa50d32bfe17ae
micattia <50896136+micattia@users.noreply.github.com>
2019-06-02 23:52:40 -0400

    Update 02_Examine_data.py

--- 02_Examine_data.py
+++ 02_Examine_data.py
@@ -15,7 +15,21 @@ dfAPPLnews.dtypes
 
 #Examine Apple data
 dfAPPLnews.describe()
+#-------------------------------------
+#Show Microsoft data head
+dfMS.head()
+
+#show data types of the Microsoft data set
+dfMS.dtypes
 
 #Examine Microsoft data
 dfMS.describe()
 
+#Show Microsoft news data head
+dfMSnews.head()
+
+#show data types of the Microsoft news data set
+dfMSnews.dtypes
+
+#Examine Microsoft news data
+dfMSnews.describe()
